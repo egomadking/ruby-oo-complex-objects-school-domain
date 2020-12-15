3055c0958fb2d461c450a154088389008ee5b00c
Roy Mosby <ego.madking@gmail.com>
2020-12-15 09:27:28 -0500

    commit tree e7a115c74268c3ccd7cc89be7b8a5e22683ce16b

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
 # code here!
+class School
+  attr_reader :roster, :name
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(student, grade)
+    if @roster.key?(grade)
+      @roster[grade] << student
+    else
+      @roster[grade] = [student]
+    end
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    intermediary = {}
+    @roster.map do |grade, students|
+      intermediary[grade] = students.sort
+    end
+    intermediary
+  end
+end
